f0411f56d8afa00d9359cd41e689293aa8d992ce
merutaso2 <merutaso2@yahoo.co.jp>
2023-07-09 17:37:44 +0900

    projects以外

--- js/scriptsEnterprise.js
+++ js/scriptsEnterprise.js
@@ -6,7 +6,7 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
-document.getElementById('submit-btn').addEventListener('click', function() {
+document.getElementById('submitButton_en').addEventListener('click', function() {
     event.preventDefault(); // デフォルトの送信動作を防止します
 
     // 新しい HTTP リクエストを作成します
@@ -14,8 +14,10 @@ document.getElementById('submit-btn').addEventListener('click', function() {
     // ここにあなたの Discord webhook URL を入力します
 	var url = 'https://discord.com/api/webhooks/1127474471360536607/aKa7qnS6UXgBIsH8G8pElWHRZ1fY-QpxZFfPlc01iZMueXcICjjPm_qrqQnR_lvOFqAP';
 	var params = JSON.stringify({
-		'content': 'つくってほしいもの：' + document.getElementById('myTextarea').value + '\n'
-  + 'メールアドレス：' + document.querySelector('input[name="newTextInput"]').value
+		'content': 'お名前：' + document.getElementById('name').value + '\n'
+  + 'メールアドレス：' + document.getElementById('email').value + '\n'
+  + '会社電話番号：' + document.getElementById('phone').value + '\n'
+  + 'お問い合わせ内容：' + document.getElementById('message').value + '\n'
 	});
     
     xhr.open('POST', url, true);
@@ -24,11 +26,12 @@ document.getElementById('submit-btn').addEventListener('click', function() {
         // リクエストの結果を処理します
         if (xhr.status === 204) { // 204 status codeは成功を意味します
             console.log('Message sent successfully');
-             alert("確認するぞい！"+ '\n'
-            +"Discordに連絡するからDMが届くように設定しててね");
+            alert("お問い合わせ頂きありがとうございます。"+ '\n'
+            +"確認でき次第、頂いたご連絡先に連絡いたしますので今しばらくお待ちください。");
+            
             clearFields();
         } else {
-              console.log('Failed to send message');
+            console.log('Failed to send message');
             alert("お問い合わせの際に何らかの問題が発生したため送信が行えておりません。確認しますのでしばしお待ちください");
             var params = JSON.stringify({
 				'content': '問い合わせに失敗しました。はくさん至急確認'
@@ -38,9 +41,11 @@ document.getElementById('submit-btn').addEventListener('click', function() {
     };
 
     xhr.send(params); // リクエストを送信します
+    
 });
     function clearFields() {
-    document.getElementById('myTextarea').value = '';
-    document.querySelector('input[name="newTextInput"]').value = '';
-
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phone').value = '';
+    document.getElementById('message').value = '';
 }
